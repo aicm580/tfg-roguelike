deda99aa2515639c1b86d705aec91bc0624902b2
aicm580 <aicm580@gmail.com>
2020-03-10 17:57:18 +0100

    Mejoras en la GENERACIÓN DE ENEMIGOS: - Añadimos el bioma al que pertenece el enemigo, para usarlo en un futuro. - Ahora se genera el número enemigos (nEnemies) adecuado en cada sala, y un enemigo se genera junto con su nº de amigos calculado.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,15 @@ public enum EnemyType
     Dilophosaurus, Meganeura,
 } 
 
+public enum Biome
+{
+    Land, Water, 
+}
+
 public class Enemy : MonoBehaviour
 {
     public EnemyType enemyType;
+    public Biome biome; 
 
     public IntRange friends; //número de enemigos igual que éste que le acompañan
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
 
         LoadLevelEnemiesArray();
         enemiesHolder = new GameObject("EnemiesHolder");
-        CreateEnemies();
+        GenerateEnemies();
     }
 
 
@@ -39,26 +39,66 @@ public class GameManager : MonoBehaviour
     }
 
 
-    void CreateEnemies()
+    void GenerateEnemies()
     {
         //Vaciamos la lista de enemigos
         enemies.Clear();
 
         for (int i = 0; i < mapGenerator.rooms.Length; i++)
         {
-            Debug.Log("Nº enemigos a generar en la sala " + i + ": " + mapGenerator.rooms[i].nEnemies);
+            CreateEnemiesInRoom(i);
             
-            Vector3 position = RandomPosition(i);
-
-            int randomEnemy = Random.Range(0, enemiesPrefabs.Count); //el máximo del Random.Range en enteros es exclusivo; el resultado máximo será enemiesPrefabs.Count-1
-
-            GameObject enemy = Instantiate(enemiesPrefabs[randomEnemy], position, Quaternion.identity) as GameObject;
-            enemy.GetComponent<Enemy>().pos = new Vector2(position.x, position.y);
-
-            enemy.transform.parent = enemiesHolder.transform;
-
-            enemies.Add(enemy);
         }
+    }
+
+    private void CreateEnemiesInRoom(int i)
+    {
+        int nEnemies = mapGenerator.rooms[i].nEnemies;
+        Debug.Log("Nº enemigos a generar en la sala " + i + ": " + nEnemies);
+
+        int enemiesCreated = 0;
+
+        while (enemiesCreated < nEnemies)
+        {
+            int randomEnemy = Random.Range(0, enemiesPrefabs.Count); //el resultado máximo será enemiesPrefabs.Count-1
+            int friends = enemiesPrefabs[randomEnemy].GetComponent<Enemy>().friends.Randomize; 
+            Biome enemyBiome = enemiesPrefabs[randomEnemy].GetComponent<Enemy>().biome;
+
+            //Si no se puede crear el enemigo junto con su nº de amigos calculado
+            if (enemiesCreated + friends + 1 > nEnemies) 
+            {
+                //Si ni siquiera poniendo el nº de amigos al mínimo cumplimos con el nEnemies calculado
+                if (enemiesCreated + enemiesPrefabs[randomEnemy].GetComponent<Enemy>().friends.minVal + 1 > nEnemies)
+                {
+                    //Pasamos a la próxima iteración sin haber creado ningún enemigo
+                    continue;
+                }
+                else
+                {
+                    //Cambiamos el nº de amigos al menor posible
+                    friends = enemiesPrefabs[randomEnemy].GetComponent<Enemy>().friends.minVal; 
+                }
+            }
+
+            for (int j = 0; j < friends + 1; j++)
+            {
+                Vector3 position;
+                if (j == 0)
+                {
+                    position = RandomPosition(i);
+                }
+                else
+                {
+                    position = RandomPosition(i);
+                }
+                
+                GameObject enemy = Instantiate(enemiesPrefabs[randomEnemy], position, Quaternion.identity) as GameObject;
+                enemy.GetComponent<Enemy>().pos = new Vector2(position.x, position.y);
+                enemy.transform.parent = enemiesHolder.transform;
+                enemies.Add(enemy);
+                enemiesCreated++;
+            }
+        }        
     }
     
 

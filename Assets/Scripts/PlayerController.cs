15ba87d92ae7e367fd6f743c9c3ac80e6a6a028a
aicm580 <aicm580@gmail.com>
2020-03-14 13:44:11 +0100

    Mejoras en el MOVIMIENTO DEL PERSONAJE: - Animaciones movimiento añadidas (faltarían animaciones diagonales) - Cambiado GetAxis a GetAxisRaw

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,24 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 4f;
     
     private Rigidbody2D rb;
+    private Animator animator;
 
     private Vector2 movement;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude); 
     }
 
     private void FixedUpdate()

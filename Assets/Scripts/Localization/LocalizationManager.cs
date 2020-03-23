b15d3fd03b94c020c7f5fefe88d8bfac375f78a5
aicm580 <aicm580@gmail.com>
2020-03-23 17:27:08 +0100

    Creamos el panel de OPCIONES y los scripts necesarios para la TRADUCCIÓN a diferentes idiomas

---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO; //lo necesitamos porque trabajaremos con archivos y su path
+
+public class LocalizationManager : MonoBehaviour
+{
+    public static LocalizationManager instance; 
+    
+    private Dictionary<string, string> localizedText;
+    private string missingText = "Key not found";
+    private bool isReady = false;
+
+    private void Awake()
+    {
+        //Nos aseguramos de que solo haya 1 LocalizationManager
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void  LoadLocalizedText(string fileName)
+    {
+        localizedText = new Dictionary<string, string>();
+
+        //Combinamos el path de la carpeta StreamingAssets con el nombre del archivo
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+
+        if(File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+
+            for (int i = 0; i < loadedData.items.Length; i++)
+            {
+                localizedText.Add(loadedData.items[i].key, loadedData.items[i].value);
+            }
+        }
+        else
+        {
+            Debug.LogError("Cannot find localization file!");
+        }
+
+        isReady = true;
+    }
+
+    public string GetLocalizedValue(string key)
+    {
+        string result = missingText;
+
+        if (localizedText.ContainsKey(key))
+        {
+            result = localizedText[key];
+        }
+
+        return result;
+    }
+
+    public bool GetIsReady()
+    {
+        return isReady;
+    }
+}
---
+++ Assets/Scripts/Localization/LocalizedText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string key; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text text = GetComponent<Text>(); //cogemos el texto del objeto
+        text.text = LocalizationManager.instance.GetLocalizedValue(key); //cambiamos el texto
+    }
+}

ad4e9ac3abf2dc6b8cdb535c46b22321a374b92d
aicm580 <aicm580@gmail.com>
2020-03-29 16:36:02 +0200

    Modificamos el panel de Stats del menú principal y modificamos su código para que la score sea la que se ha guardado mediante el SaveManager

--- Assets/Scripts/StatsPanelManager.cs
+++ Assets/Scripts/StatsPanelManager.cs
@@ -5,7 +5,18 @@ using UnityEngine.UI;
 
 public class StatsPanelManager : MonoBehaviour
 {
-    public Text damageDoneText;
+    public Text damageScore;
+    public Text totalDeathsScore;
+    public Text deathsByBossScore;
+    public Text deathsByEnemiesScore;
+    public Text totalKillsScore;
+    public Text normalKillsScore;
+    public Text bossKillsScore;
+    public Text winsScore;
+    public Text travelsScore;
+    public Text timePlayedScore;
+    public Text maxLevelScore;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +25,17 @@ public class StatsPanelManager : MonoBehaviour
        
         if (stats != null)
         {
-            damageDoneText.text += ": " + stats.damageDone;
-        }
-        else
-        {
-            damageDoneText.text += ": 0";
+            damageScore.text = stats.damageDone.ToString();
+            totalDeathsScore.text = stats.totalDeaths.ToString();
+            deathsByBossScore.text = stats.deathsByBoss.ToString();
+            deathsByEnemiesScore.text = stats.deathsByNormalEnemies.ToString();
+            totalKillsScore.text = stats.totalKills.ToString();
+            normalKillsScore.text = stats.normalEnemiesKilled.ToString();
+            bossKillsScore.text = stats.bossesKilled.ToString();
+            winsScore.text = stats.wins.ToString();
+            travelsScore.text = stats.travels.ToString();
+            timePlayedScore.text = stats.timePlayed.ToString();
+            maxLevelScore.text = stats.maxLevelReached.ToString();
         }
     }
 

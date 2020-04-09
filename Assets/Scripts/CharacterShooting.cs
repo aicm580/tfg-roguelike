5f98fc27db5c74bee941d5dff062f05a9505ee7a
aicm580 <aicm580@gmail.com>
2020-04-09 13:43:39 +0200

    Continuamos con la REORGANIZACIÓN del código del jugador

---
+++ Assets/Scripts/CharacterShooting.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CharacterShooting : MonoBehaviour
+{
+    [SerializeField]
+    protected float shootDelay = 0.2f;
+    [SerializeField]
+    protected GameObject bulletPrefab;
+
+    public void Shoot(Vector3 originPosition, Quaternion rotation)
+    {
+        Instantiate(bulletPrefab, originPosition, rotation);
+    }
+}
--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -3,8 +3,16 @@
 [RequireComponent(typeof(CharacterMovement))]
 public class PlayerInputController : MonoBehaviour
 {
+    public Transform firePoint;
+
     private CharacterMovement characterMovement;
+    private CharacterShooting characteShooting;
+
     private Vector2 movement;
+    private Vector2 mousePos;
+    private Vector2 firepointPos;
+    private Vector2 lookDirection;
+    private Vector3 bulletOrigin = new Vector3();
     private Animator animator;
 
     void Awake()
@@ -15,12 +23,26 @@ public class PlayerInputController : MonoBehaviour
 
     void Update()
     {
+        //KEYBOARD INPUT MANAGEMENT
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        //MOUSE INPUT MANAGEMENT
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        animator.SetFloat("Horizontal", lookDirection.x);
+        animator.SetFloat("Vertical", lookDirection.y);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            characteShooting.Shoot(bulletOrigin, Quaternion.identity);
+        }
     }
 
     private void FixedUpdate()
     {
         characterMovement.Move(movement);
+
+        firepointPos = new Vector2(firePoint.position.x, firePoint.position.y);
+        lookDirection = (mousePos - firepointPos).normalized;
+        bulletOrigin = firePoint.position + (Vector3)(lookDirection * 0.5f);
     }
 }

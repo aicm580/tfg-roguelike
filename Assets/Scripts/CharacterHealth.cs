b0fb67cfa1d819c875957b41a81caa7aa3050529
aicm580 <aicm580@gmail.com>
2020-04-09 17:56:18 +0200

    Continuamos con la REORGANIZACIÓN del código del player

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+    [SerializeField]
+    protected int initHealth;
+    
+    protected int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = initHealth;
+    }
+
+    public void TakeDamage(int dmgAmount)
+    {
+        currentHealth -= dmgAmount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : CharacterHealth
+{
+    [SerializeField]
+    protected int initHearts = 3; //nº de corazones iniciales (llenos o no)
+    [SerializeField]
+    protected int maxHearts = 10; //nº de corazones máximo que puede recolectar
+
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        if (currentHealth > maxHearts)
+        {
+            currentHealth = maxHearts;
+        }
+    }
+}

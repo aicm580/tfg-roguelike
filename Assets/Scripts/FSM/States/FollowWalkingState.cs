218a1298429e59718d2df909d21d849f47b13656
aicm580 <aicm580@gmail.com>
2020-04-17 14:37:04 +0200

    Seguimos con el Follow Walking State de los enemigos

--- Assets/Scripts/FSM/States/FollowWalkingState.cs
+++ Assets/Scripts/FSM/States/FollowWalkingState.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Move
+{
+    Right, Left, Up, Down,
+}
+
 public class FollowWalkingState : State
 {
     Vector2 direction;
@@ -14,6 +19,7 @@ public class FollowWalkingState : State
     bool leftHit;
     bool topHit;
     bool bottomHit;
+    Move lastMove;
 
     public FollowWalkingState(Enemy enemy, StateType state) : base(enemy, state) { }
 
@@ -29,7 +35,6 @@ public class FollowWalkingState : State
         if (enemy.NeedChangeState(enemy.attackRange, masks))
         {
             // enemy.fsm.EnterNextState();
-            Debug.Log("Ataquemos");
         }
         //Si no está en el rango de ataque, el enemigo debe acercarse al jugador
         else
@@ -42,35 +47,59 @@ public class FollowWalkingState : State
             else //algo bloquea el camino más corto hacia el jugador
             {
                 direction = Vector2.zero;
-                rightHit = Physics2D.Raycast(enemy.rightRayOrigin.position, Vector2.right, 0.5f, masks);
-                leftHit = Physics2D.Raycast(enemy.leftRayOrigin.position, Vector2.left, 0.5f, masks);
-                topHit = Physics2D.Raycast(enemy.topRayOrigin.position, Vector2.up, 0.5f, masks);
-                bottomHit = Physics2D.Raycast(enemy.bottomRayOrigin.position, Vector2.down, 0.5f, masks);
+                rightHit = Physics2D.Raycast(enemy.rightRayOrigin.position, Vector2.right, 0.4f, masks);
+                leftHit = Physics2D.Raycast(enemy.leftRayOrigin.position, Vector2.left, 0.4f, masks);
+                topHit = Physics2D.Raycast(enemy.topRayOrigin.position, Vector2.up, 0.4f, masks);
+                bottomHit = Physics2D.Raycast(enemy.bottomRayOrigin.position, Vector2.down, 0.4f, masks);
 
                 float enemyPosX = enemy.transform.position.x;
                 float enemyPosY = enemy.transform.position.y;
                 float targetPosX = enemy.target.position.x;
                 float targetPosY = enemy.target.position.y;
-
-
-                if (targetPosX >= enemyPosX + 0.5f)
+                
+                if (targetPosX >= enemyPosX + 0.1f && !rightHit && lastMove != Move.Left)
                 {
-                    Debug.Log("está a la derecha");
-                    if (!rightHit)
-                        direction = Vector2.right;
-                    else if (targetPosY >= enemyPosY && !topHit)
-                        direction = Vector2.up;
-                    else if (targetPosY < enemyPosY && !bottomHit)
-                        direction = Vector2.down;
+                    direction = Vector2.right;
+                    lastMove = Move.Right;    
                 }
-                else if (targetPosX < enemyPosX && !leftHit)
+                else if (targetPosY >= enemyPosY && !topHit && lastMove != Move.Down)
                 {
-                    if (!leftHit)
+                    direction = Vector2.up;
+                    lastMove = Move.Up;
+                }
+                else if (targetPosX < enemyPosX - 0.1f && !leftHit && lastMove != Move.Right)
+                {
+                    direction = Vector2.left;
+                    lastMove = Move.Left;
+                }
+                else if (targetPosY < enemyPosY && !bottomHit && lastMove != Move.Up)
+                {
+                    direction = Vector2.down;
+                    lastMove = Move.Down;
+                }
+                
+                if (direction == Vector2.zero)
+                {
+                    if (!leftHit && lastMove != Move.Right)
+                    {
                         direction = Vector2.left;
-                    else if (targetPosY >= enemyPosY && !topHit)
+                        lastMove = Move.Left;
+                    }
+                    else if (!topHit && lastMove != Move.Down)
+                    {
                         direction = Vector2.up;
-                    else if (targetPosY < enemyPosY && !bottomHit)
+                        lastMove = Move.Up;
+                    }
+                    else if (!rightHit)
+                    {
+                        direction = Vector2.right;
+                        lastMove = Move.Right;
+                    }
+                    else if (!bottomHit)
+                    {
                         direction = Vector2.down;
+                        lastMove = Move.Down;
+                    }     
                 }
             }
         }
